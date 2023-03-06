5a61b6a872f84f97e4df4cb0e70c9adc8cfb5174
MestreWalla <mayconvitorcorrea@gmail.com>
2023-03-06 20:43:23 -0300

    Update

--- ExerciciosIfElse/JAVA/ExerciciosIfElse/Exercicios.java
+++ ExerciciosIfElse/JAVA/ExerciciosIfElse/Exercicios.java
@@ -150,3 +150,35 @@ public class Exercicios {
         System.out.println("O numero é impar");
     }
     }
+
+// 2- Desafio 1: Crie um programa que recebe uma nota ( pela classe Scanner) e checa se você passou direto, ficou de recuperação ou foi reprovado na matéria.
+// A regra é a seguinte:
+// Nota 7 ou mais: passou direto
+// Entre 5 e 7: têm direito de fazer uma prova de recuperação
+// Abaixo de 5: reprovado direto
+// 3- Faça um programa que verifique (usando if e else) se uma letra digitada é vogal ou consoante.
+// *Difícil:
+// 4- Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, baseado no salário atual:
+// -Salários até R$ 1280,00 (incluindo): aumento de 20%;
+// -Salários entre R$ 1280,00 e R$1700,00: aumento de 15%;
+// -Salários entre R$ 1700,00 e R$ 2500,00: aumento de 10%;
+// -Salários de R$ 2500,00 em diante: aumento de 5% 
+// Após o aumento ser realizado; informe na tela:
+// *O salário antes do reajuste;
+// *O percentual de aumento aplicado;
+// *O valor do aumento;
+// *O novo salário, após o aumento.  
+// 5- Faça um programa para o cálculo de uma folha de pagamento, sabendo que os descontos são do imposto de Renda, que depende do salário bruto (conforme tabela abaixo) e 3% para o Sindicato e que o FGTS corresponde a 11% do salário bruto, mas não é descontado (é a empresa que deposita.)
+// O salário líquido corresponde ao salário bruto menos os descontos O programa deverá pedir ao usuário o valor da sua hora e a quantidade de horas trabalhadas no mês.
+// a. Desconto do IR;
+// b. Salário Bruto até R$900,00 (inclusive) – Isento;
+// c. Salário Bruto de R$ 1500, 00 (inclusive) – desconto de 5%;
+// d. Salário bruto até R$ 2500,00 (Inclusive) – desconto de 10%;
+// e. Salário bruto acima de 2500 – Desconto de 20%.
+// Imprima na tela as informações, dispostas conforme o exemplo abaixo, no exemplo valor da hora é 5 e a quantidade de horas é 220.  
+// Salário bruto (5 * 220)           : R$   1100,00
+// ( – ) IR (5%)                     : R$     55,00
+// ( – ) INSS ( 10% )                 : R$     110,00
+// FGTS ( 11%)                       : R$     121,00
+// Total de descontos                 : R$     165,00
+// Salário Líquido                   : R$     935,00 
