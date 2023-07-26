4e429e958f0097fa951983289ccfc6c8eaf34467
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2023-07-25 22:02:00 -0300

    update

--- Semestre02/Aula01RevisaoFPOO/App.java
+++ Semestre02/Aula01RevisaoFPOO/App.java
@@ -35,6 +35,18 @@ public class App {
                                     + "\n 2-Cadastrar Gato"
                                     + "\n 3-Cadastrar Outro Especime"
                                     + "\n 4-Voltar"));
+                                    if (acao == 1) {
+                                        if (contC < animalCachorro.length) {
+                                            animalCachorro[contC] = new Cachorro(null, null, acaoCadastro, null, null, null, null, null);
+                                            animalCachorro[contC].setTipoAnimal(JOptionPane.showInputDialog("Informe o tipo:"));
+                                            animalCachorro[contC].setNome(JOptionPane.showInputDialog("Informe o Nome do animal:"));
+                                            animalCachorro[contC].setEspecie(JOptionPane.showInputDialog("Informe a especie:"));
+                                            animalCachorro[contC].setSexo(JOptionPane.showInputDialog("Informe o sexo:"));
+                                            animalCachorro[contC].setRaca(JOptionPane.showInputDialog("Informe a Raça:"));
+                                            animalCachorro[contC].setCorPredominante(JOptionPane.showInputDialog("Informe a cor predominante:"));
+                                            animalCachorro[contC].setProprietario(JOptionPane.showInputDialog("Informe o proprietário:"));
+                                        }
+                                    }
                 }
             } else if (acao == 2) { // Consultar Animal
                 boolean consulta = true;
