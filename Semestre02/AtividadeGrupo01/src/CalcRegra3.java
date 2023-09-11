a6de477029d49a16647dd81bfa6657ee6ae55b3d
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2023-09-11 20:44:47 -0300

    Update CalcRegra3.java

--- Semestre02/AtividadeGrupo01/src/CalcRegra3.java
+++ Semestre02/AtividadeGrupo01/src/CalcRegra3.java
@@ -13,33 +13,32 @@ public class CalcRegra3 extends JPanel {
     // =========================Calculadora Regra de 3 =========================//
     public CalcRegra3() {
         super(new BorderLayout());
-        // Criar Frame principal onde ficaram todos os outros JPanel
-        JPanel frame = new JPanel(new BorderLayout());
-        //
+
+        JPanel frame = new JPanel(new BorderLayout()); // Criar Frame principal onde ficaram todos os outros JPanel
+
         // ============================Painel superior (primeiropainel)================
-        //
         JPanel painel1 = new JPanel(new BorderLayout());
         painel1.setBackground(Color.decode("#03dbfc"));
         // criar caixa para inserir o primeiro valor
         JTextField campoTextoA = new JTextField("A", 25);
         campoTextoA.setHorizontalAlignment(SwingConstants.CENTER); // Centraliza o texto
         campoTextoA.setBackground(Color.decode("#127382"));
-        this.add(campoTextoA);
+        painel1.add(campoTextoA, BorderLayout.NORTH);
         // Texto para ficar entre os dois primeros campos
         JLabel texto1 = new JLabel("Está para");
-        this.add(texto1);
+        painel1.add(texto1, BorderLayout.CENTER);
         // criar caixa para inserir o segundo valor
         JTextField campoTextoB = new JTextField("B", 25);
         campoTextoB.setHorizontalAlignment(SwingConstants.CENTER); // Centraliza o texto
         campoTextoB.setBackground(Color.decode("#127382"));
-        this.add(campoTextoB);
+        painel1.add(campoTextoB, BorderLayout.SOUTH);
         //
         // ========================Painel intermediario "Assim Como"==================
         //
         JPanel painel2 = new JPanel();
         painel2.setBackground(Color.decode("#03dbfc"));
         JLabel texto2 = new JLabel("Assim Como");
-        this.add(texto2);
+        painel2.add(texto2);
         //
         // =======================Painel inferior (terceiro painel)===================
         //
@@ -49,10 +48,10 @@ public class CalcRegra3 extends JPanel {
         JTextField campoTextoC = new JTextField("C", 25);
         campoTextoC.setHorizontalAlignment(SwingConstants.CENTER); // Centraliza o texto
         campoTextoC.setBackground(Color.decode("#127382"));
-        this.add(campoTextoC);
+        painel3.add(campoTextoC);
         // Texto para ficar entre os dois ultimos campos
         JLabel texto3 = new JLabel("Está para");
-        this.add(texto3);
+        painel3.add(texto3);
         // ==============================Resultado
         JTextField resultado = new JTextField("Resultado");
         resultado.setBackground(Color.decode("#a7e5eb"));
@@ -64,13 +63,10 @@ public class CalcRegra3 extends JPanel {
         // Botão para realizar o cálculo
         JButton calcular = new JButton("Calcular");
         calcular.setBackground(Color.decode("#127382"));
-        this.add(calcular);
-        //
-        // ============================================================================
-        //
+        this.add(calcular, BorderLayout.EAST);
+
         // ==========================Realizar os Calculos===============================
-        //  
-        //
+
         calcular.addActionListener(null); // Adiciona ação ao botão
         calcular.addActionListener(e -> { // o que fazer quando o botaão for selecionado
             // Pega os valores digitados
@@ -82,9 +78,9 @@ public class CalcRegra3 extends JPanel {
             // Exibe o resultado no JTextList solucao
             resultado.setText(Double.toString(solucao));
         });
-        frame.add(painel1);
-        frame.add(painel2);
-        frame.add(painel3);
+        frame.add(painel1, BorderLayout.NORTH);
+        frame.add(painel2, BorderLayout.CENTER);
+        frame.add(painel3, BorderLayout.SOUTH);
         frame.setSize(310, 330);
         frame.setVisible(true);
     }
