57a1c22e6877d2e826a8a14f4c527de52063ae96
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2023-10-18 20:52:38 -0300

    Update TodoList.java

--- Semestre02/SituacaoDeAprendizagem04TodoList/src/TodoList.java
+++ Semestre02/SituacaoDeAprendizagem04TodoList/src/TodoList.java
@@ -1,12 +1,15 @@
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.FlowLayout;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TimerTask;
+import java.util.Timer;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -29,8 +32,10 @@ public class TodoList extends JFrame {
     private CardLayout cl;
     private JPanel Inicial;
     private JPanel tempPanel;
+    private JPanel tempPanelCompnents;
     private JTextField taskInputField;
     private JTextField taskInputField2;
+    private JTextField tempo;
     private JButton addButton;
     private JButton addButton2;
     private JButton addTimer;
@@ -48,26 +53,31 @@ public class TodoList extends JFrame {
         // Configuração da janela principal
         super("To-Do List App");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(500, 300);
+        this.setSize(400, 300);
+        this.setLocation(((Toolkit.getDefaultToolkit().getScreenSize().width / 2) - (this.getWidth() / 2)),
+                ((Toolkit.getDefaultToolkit().getScreenSize().height / 2) - (this.getHeight() / 2)));
         AplicaNimbusLookAndFeel.pegaNimbus();
 
         // Inicializa os paineis
         mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout());
-            // Inicializa os cards
-            cl = new CardLayout(); // criar o objeto do cardLayout
-            cards = new JPanel(cl);
-            mainPanel.add(cards, BorderLayout.NORTH); // add o JPanel "cards" ao painel principal
-                // Inicializa o painel inicial
-                Inicial = new JPanel();
-                Inicial.setLayout(new BorderLayout());
-                cards.add(Inicial, "Inicial");
-                Inicial.setSize(500, 300);
+        // Inicializa os cards
+        cl = new CardLayout(); // criar o objeto do cardLayout
+        cards = new JPanel(cl);
+        mainPanel.add(cards, BorderLayout.NORTH); // add o JPanel "cards" ao painel principal
+        // Inicializa o painel inicial
+        Inicial = new JPanel();
+        Inicial.setLayout(new BorderLayout());
+        cards.add(Inicial, "Inicial");
+        Inicial.setSize(500, 300);
 
-                // Inicializa o painel temporizador
-                tempPanel = new JPanel();
-                tempPanel.setLayout(new BorderLayout());
-                cards.add(tempPanel, "Temporizador");
+        // Inicializa o painel temporizador
+        tempPanel = new JPanel();
+        tempPanelCompnents = new JPanel();
+        tempPanel.setLayout(new BorderLayout());
+        tempPanelCompnents.setLayout(new BorderLayout());
+
+        cards.add(tempPanel, "Temporizador");
 
         // Inicializa a lista de tasks e a lista de tasks concluídas
         tasks = new ArrayList<>();
@@ -76,7 +86,8 @@ public class TodoList extends JFrame {
 
         // Inicializa campos de entrada, botões e JComboBox
         taskInputField = new JTextField();
-        taskInputField2 = new JTextField(); 
+        taskInputField2 = new JTextField();
+        tempo = new JTextField();
         addButton = new JButton("Adicionar");
         addButton2 = new JButton("Adicionar");
         addTimer = new JButton("Timer");
@@ -97,6 +108,7 @@ public class TodoList extends JFrame {
         markDoneButton.setToolTipText("Marcar tarefa selecionada como concluida");
         filterComboBox.setToolTipText("Mudar modo de exibição da lista entre as tarefas Ativas, Concluidas e Ativas");
         clearCompletedButton.setToolTipText("Excluir todas as tarefas concluidas");
+        tempo.setToolTipText("Tempo a ser atrazado em segundos");
 
         // Configuração do painel de entrada
         JPanel inputPanel = new JPanel(new BorderLayout());
@@ -116,17 +128,18 @@ public class TodoList extends JFrame {
         inputPanel2.add(taskInputField2, BorderLayout.CENTER);
         inputPanel2.add(voltar, BorderLayout.WEST);
         // inputPanel2.add(taskInputField, BorderLayout.CENTER);
-        
+
         // Adiciona os componentes ao painel Inicial
         Inicial.add(inputPanel, BorderLayout.NORTH);
         Inicial.add(new JScrollPane(taskList), BorderLayout.CENTER);
         Inicial.add(buttonPanel, BorderLayout.SOUTH);
 
         // Adiciona os componenter ao tempPanel
-        tempPanel.add(new JLabel("Marque o tempo de atrazo para adicionar a tarefa:"), BorderLayout.WEST);
-        tempPanel.add(new JTextField("01 min", 20), BorderLayout.CENTER);
-        tempPanel.add(addButton2, BorderLayout.EAST);
         tempPanel.add(inputPanel2, BorderLayout.NORTH);
+        tempPanel.add(tempPanelCompnents, BorderLayout.CENTER);
+        tempPanelCompnents.add(new JLabel("Marque o tempo de atrazo para adicionar a tarefa:"), BorderLayout.NORTH);
+        tempPanelCompnents.add(tempo, BorderLayout.CENTER);
+        tempPanelCompnents.add(addButton2, BorderLayout.EAST);
 
         // Adiciona o painel Inicial à janela
         this.add(mainPanel);
@@ -139,6 +152,16 @@ public class TodoList extends JFrame {
             public void keyPressed(KeyEvent arg0) {
                 if (arg0.getKeyCode() == KeyEvent.VK_ENTER) {
                     addTask();
+                }
+            }
+        });
+
+        tempo.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent arg0) {
+                if (arg0.getKeyCode() == KeyEvent.VK_ENTER) {
+                    addTask2();
+                    cl.show(cards, "Inicial");
                 }
             }
         });
@@ -164,10 +187,13 @@ public class TodoList extends JFrame {
             // Inicio.setEnabled(true);
         });
 
-        addTimer.addActionListener(new ActionListener() {
+        addButton2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                addTask();
+                addTask2();
+                cl.show(cards, "Inicial");
+                taskInputField2.setText("");
+                tempo.setText("");
             }
         });
         addButton.addActionListener(new ActionListener() {
@@ -213,12 +239,41 @@ public class TodoList extends JFrame {
             updateTaskList();
             taskInputField.setText("");
 
+        } else {
+            JOptionPane.showMessageDialog(null, "Digite um texto para adicionar uma tarefa.");
         }
+    }
+
+    private void addTask2() {
+        String taskDescription = taskInputField2.getText().trim();
+
+        if (!taskDescription.isEmpty()) {
+            Timer timer = new Timer();
+            try {
+                int seconds = Integer.parseInt(tempo.getText().trim());
+                timer.schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        Task newTask = new Task(taskDescription);
+                        tasks.add(newTask);
+                        updateTaskList();
+                        taskInputField2.setText("");
+                    }
+                }, seconds * 1000); // Agendando a tarefa para o número de segundos especificado
+            } catch (NumberFormatException erro1) {
+                JOptionPane.showMessageDialog(null, "Erro - Valor digitado invalido");
+            } finally {
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Digite um texto para adicionar uma tarefa.");
+        }
+
     }
 
     private void deleteTask() {
         // Exclui a task selecionada da lista de tasks
         int selectedIndex = taskList.getSelectedIndex();
+
         if (selectedIndex >= 0 && selectedIndex < tasks.size()) {
             Object[] options = { "   Sim   ", "   Não   " };
             int n = JOptionPane.showOptionDialog(null, " Deseja realmente excluir a tarefa?", "Confirmar exclusão?",
@@ -228,6 +283,8 @@ public class TodoList extends JFrame {
                 // System.exit(0);
             }
             updateTaskList();
+        } else {
+            JOptionPane.showMessageDialog(null, "Selecione uma tarefa antes de executar a ação", "Erro", selectedIndex);
         }
     }
 
@@ -238,6 +295,9 @@ public class TodoList extends JFrame {
             Task task = tasks.get(selectedIndex);
             task.setDone(true);
             updateTaskList();
+        } else {
+            JOptionPane.showMessageDialog(null, "Selecione uma tarefa antes de executar a ação", "Erro", selectedIndex);
+
         }
     }
 
@@ -256,18 +316,24 @@ public class TodoList extends JFrame {
     private void clearCompletedTasks() {
         // Limpa todas as tasks concluídas da lista
         List<Task> completedTasks = new ArrayList<>();
+
+        Object[] options = { "   Sim   ", "   Não   " };
+        int n = JOptionPane.showOptionDialog(null, " Deseja realmente excluir todas as tarefas concluidas?",
+                "Confirmar exclusão de concluido?",
+                JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[1]);
+        int cont = 0;
         for (Task task : tasks) {
+
             if (task.isDone()) {
-
-                Object[] options = { "   Sim   ", "   Não   " };
-                int n = JOptionPane.showOptionDialog(null, " Deseja realmente excluir todas as tarefas concluidas?",
-                        "Confirmar exclusão de concluido?",
-                        JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[1]);
-
                 if (n == 0) {
                     completedTasks.add(task);
+                    cont++;
                 }
             }
+        }
+        if (cont == 0) {
+            JOptionPane.showMessageDialog(null, "Não ha nenhuma tarefa concluida.");
+            cont = 0;
         }
         tasks.removeAll(completedTasks);
         updateTaskList();
