25712ba8fb41941fc9a115a7a818cc28f036e966
Maycon Vitor Correa <105678497+MestreWalla@users.noreply.github.com>
2023-11-06 21:55:22 -0300

    update

--- Semestre02/jdbcconnectionteste/src/main/java/app/Main.java
+++ Semestre02/jdbcconnectionteste/src/main/java/app/Main.java
@@ -1,18 +1,44 @@
 package app;
 
+import java.util.Scanner;
+
 import Connection.ConnectionDAO;
 
 public class Main {
     public static void main(String[] args) {
-        new ConnectionDAO().apagarTabela();
-        int escolha;
+        // new ConnectionDAO().apagarTabela();
+
+        System.out.println("Escolha o exercício que deseja executar:");
+        System.out.println("01 - Criar Tabela");
+        System.out.println("02 - Inserir na tabela");
+        System.out.println("03 - Buscar na tabela");
+        System.out.println("04 - Apagar tabela");
+        Scanner scanner = new Scanner(System.in);
+        int escolha = scanner.nextInt();
         switch (escolha) {
-            case value:
-                
+            case 1:
+                new ConnectionDAO().criarTabela();
+                ;
                 break;
-        
+            case 2:
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Informe o nome:");
+                String nome = sc.nextLine();
+                System.out.println("Informe o e-mail:");
+                String eMail = sc.nextLine();
+                new ConnectionDAO().inserir(nome, eMail);
+                break;
+            case 3:
+                new ConnectionDAO().buscarPorId(escolha);
+                ;
+                break;
+            case 4:
+                new ConnectionDAO().apagarTabela();
+                break;
             default:
+                System.out.println("Opção Invalida");
                 break;
         }
+        scanner.close();
     }
 }
